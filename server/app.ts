ba4ed6f9275426979c52b5fc15e438f9f6121d70
unknown <kwaifss@gmail.com>
2023-01-16 21:03:22 -0600

    test

--- server/app.ts
+++ server/app.ts
@@ -43,8 +43,8 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get("/", (req, res) => {
-  res.send("what");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
 });
 
 app.use('/leagues', leagueRouter);
