7d06c5353c4c9d4bb1be8d6fd85466efa04d200c
unknown <kwaifss@gmail.com>
2023-01-17 17:38:09 -0600

    serve static files

--- server/app.ts
+++ server/app.ts
@@ -42,9 +42,9 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 app.use('/leagues', leagueRouter);
 app.use('/sports', sportRouter);
